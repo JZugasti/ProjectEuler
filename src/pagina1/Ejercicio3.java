3234c3421df93631681fd52fc3b12126b6a2517a
Javier Zugasti Gascón <zugasti.javier@gmail.com>
2026-03-05 20:51:12 +0100

    Pruebas

--- src/pagina1/Ejercicio3.java
+++ src/pagina1/Ejercicio3.java
@@ -6,6 +6,12 @@ public class Ejercicio3 {
 
         //calcular el factor primo más grande 600851475143
 
+        for (double i= 1; i<600851475143/2;i++)  {
+
+
+
+        }
+
     } //Cierre main
 
 } //Cierre classe
