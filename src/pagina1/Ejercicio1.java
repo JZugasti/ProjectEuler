760a75539e4f168af838a7e4118507a2d822007e
Javier Zugasti Gascón <jzg@gmail.com>
2026-03-05 20:32:53 +0100

    Ejercicio 1. Creación archivo 3

--- src/pagina1/Ejercicio1.java
+++ src/pagina1/Ejercicio1.java
@@ -7,6 +7,20 @@ public class Ejercicio1 {
 
 		//Suma de los números multiplos de 3 y 5 que hay dentro de los primeros 1000 números
 
+		double suma = 0;
+
+		for (int i=3; i<1000;i++) {
+
+			if (i%3 == 0 || i%5 ==0) {
+
+				suma = suma + i;
+
+			}
+
+		}
+
+		System.out.println("El valor de los multiplos de 3 y 5 en los primeros 1000 números es: " + suma);
+
 	} //Cierre main
 
 } //Cierre class
---
+++ src/pagina1/Ejercicio3.java
@@ -0,0 +1,11 @@
+package pagina1;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+
+        //calcular el factor primo más grande 600851475143
+
+    } //Cierre main
+
+} //Cierre classe
